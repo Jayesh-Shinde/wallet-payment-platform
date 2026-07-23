5fd0d394f150890185c863984fe7bc55bce4b58a
Jayesh Shinde <jayeshshinde@Jayeshs-MacBook-Pro.local>
2026-07-23 17:35:22 +0900

    fix: conflicts

--- src/main/java/com/jayeshshinde/walletpaymentplatform/dtos/TransferInputDTO.java
+++ src/main/java/com/jayeshshinde/walletpaymentplatform/dtos/TransferInputDTO.java
@@ -5,21 +5,10 @@ import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
-//@AllArgsConstructor
-//@Data
-//@NoArgsConstructor
-//@Builder
-//public class TransferInputDTO {
-//    @NotNull
-//    public UUID fromWalletId;
-//    @NotNull
-//    public UUID toWalletId;
-//    @Min(value = 1)
-//    public Long amount;
-//}
-
-public record TransferInputDTO(@NotNull UUID fromWalletId,
-                               @NotNull UUID toWalletId,
-                               @Min(value = 1) Long amount) {
+public record TransferInputDTO(
+        @NotNull UUID fromWalletId,
+        @NotNull UUID toWalletId,
+        @Min(value = 1) Long amount
+) {
 }
 
--- src/main/java/com/jayeshshinde/walletpaymentplatform/dtos/TransferOutputDTO.java
+++ src/main/java/com/jayeshshinde/walletpaymentplatform/dtos/TransferOutputDTO.java
@@ -1,23 +1,5 @@
 package com.jayeshshinde.walletpaymentplatform.dtos;
 
-//@AllArgsConstructor
-//@Data
-//@NoArgsConstructor
-//@Builder
-//public class TransferOutputDTO {
-//
-//    public UUID id;
-//
-//    public UUID fromWalletId;
-//
-//    public UUID toWalletId;
-//
-//    public Long amount;
-//
-//    public TransferStatus status;
-//    public String reason;
-//}
-
 import com.jayeshshinde.walletpaymentplatform.enums.TransferStatus;
 import jakarta.validation.constraints.NotNull;
 
