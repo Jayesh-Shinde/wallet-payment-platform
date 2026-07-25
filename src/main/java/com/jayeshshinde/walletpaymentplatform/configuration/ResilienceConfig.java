64e950efc89fa5098e5b9ebe1c94442a065ed0ff
Jayesh Shinde <jayeshshinde@Jayeshs-MacBook-Pro.local>
2026-07-25 13:27:09 +0900

    feat: idempotency test case added

---
+++ src/main/java/com/jayeshshinde/walletpaymentplatform/configuration/ResilienceConfig.java
@@ -0,0 +1,9 @@
+package com.jayeshshinde.walletpaymentplatform.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.resilience.annotation.EnableResilientMethods;
+
+@Configuration
+@EnableResilientMethods
+public class ResilienceConfig {
+}
--- src/test/java/com/jayeshshinde/walletpaymentplatform/component/FundingServiceTest.java
+++ src/test/java/com/jayeshshinde/walletpaymentplatform/component/FundingServiceTest.java
@@ -3,6 +3,7 @@ package com.jayeshshinde.walletpaymentplatform.component;
 import com.jayeshshinde.walletpaymentplatform.dtos.TransferInputDTO;
 import com.jayeshshinde.walletpaymentplatform.dtos.TransferOutputDTO;
 import com.jayeshshinde.walletpaymentplatform.enums.TransferStatus;
+import com.jayeshshinde.walletpaymentplatform.service.FundingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,8 +19,8 @@ class FundingServiceTest {
     @Test
     void transferFunds() {
         UUID fromWalletId = UUID.fromString("cf3b1447-6270-4637-ad78-f1ca79c87374");
-        UUID toWalletId = UUID.fromString("a09e973b-27d2-43ff-84d6-ed2cae7d94da");
-        TransferInputDTO transferInputDTO = new TransferInputDTO(fromWalletId, toWalletId, 10L);
+        UUID toWalletId = UUID.fromString("fa247d06-52fd-47f0-a70e-80317d2fec64");
+        TransferInputDTO transferInputDTO = new TransferInputDTO(fromWalletId, toWalletId, 1000000L);
         UUID idempotencyKey = UUID.randomUUID();
         TransferOutputDTO transferOutputDTO = fundingService.transferFunds(transferInputDTO, idempotencyKey);
         assert transferOutputDTO.status().equals(TransferStatus.COMPLETED);
