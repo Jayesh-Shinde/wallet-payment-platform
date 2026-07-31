c97a09903b356a7239707007b8a204399d44c709
Jayesh Shinde <40816261+Jayesh-Shinde@users.noreply.github.com>
2026-07-31 17:16:54 +0900

    feat: kafka event outbox pattern first draft (#12)
    Co-authored-by: Jayesh Shinde <jayeshshinde@Jayeshs-MacBook-Pro.local>

---
+++ src/main/java/com/jayeshshinde/walletpaymentplatform/enums/EventTransferStatus.java
@@ -0,0 +1,8 @@
+package com.jayeshshinde.walletpaymentplatform.enums;
+
+public enum EventTransferStatus {
+    PENDING,
+    PROCESSED,
+    FAILED,
+    DEAD_LETTER
+}
---
+++ src/main/java/com/jayeshshinde/walletpaymentplatform/enums/EventTransferType.java
@@ -0,0 +1,6 @@
+package com.jayeshshinde.walletpaymentplatform.enums;
+
+public enum EventTransferType {
+    TRANSFER_COMPLETE,
+    TRANSFER_FAILED,
+}
